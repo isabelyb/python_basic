aa194cfa39a485f7776657af0b9292d6b329bdc3
Isabel <isabelcristinayepesb@gmail.com>
2020-09-30 22:35:07 +0100

    primer programa en py

---
+++ conversor_monedas.py
@@ -0,0 +1,19 @@
+
+
+
+cop = input("Cuantos pesos colombianos tienes?")
+cop = float(cop)
+valor_usd = 3875
+usd = cop / valor_usd
+usd = round(usd, 2)
+usd = str(usd)
+print("Tienes $" + usd + " USD")
+
+dolar = input("Cuantos dolares tienes?")
+dolar = float(dolar)
+valor_dolar = 3875
+pesos = dolar * valor_dolar
+pesos = round(pesos, 2)
+pesos = str(pesos)
+print("Tienes $" + pesos + " pesos colombianos")
+
