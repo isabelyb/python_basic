71e78d856a9e97d6b764958acd7e2251a392bb9d
isabelyb <isabelcristinayepesb@gmail.com>
2021-05-07 14:34:30 -0500

    rapasando Python1

--- 01.condicionales.py
+++ 01.condicionales.py
@@ -1,8 +1,8 @@
-#edad = int(input("Escribe tu edad: "))
-#if edad > 17:
-#	print("Eres mayor de edad")
-#else:
-#	print("Eres menor de edad")
+# edad = int(input("Escribe tu edad: "))
+# if edad > 17:
+# 	print("Eres mayor de edad")
+# else:
+# 	print("Eres menor de edad")
 
 numero = int(input("Escribe un numero: "))
 if numero > 5:
--- strings.py
+++ strings.py
@@ -5,3 +5,10 @@ def run():
 
 if __name__ == "__main__":
 	run()
+
+
+# variable.upper() = 'todos los caracteres en MAYÚSCULAS'
+# variable.capitalize() = 'solo la primera en MAYÚSCULA'
+# variable.lower() = 'todos los caracteres en minúscula'
+# variable.strip() = 'eliminar espacios basura del string'
+# variable.replace('caractera a cambiar', 'caracter por poner') = remplazar caracter
