3cbc29f449960572015d31bb522d89ad60b80af8
isabelyb <isabelcristinayepesb@gmail.com>
2021-06-15 13:53:45 -0500

    Herencia y Polimorfismo

--- POO/11_herencia.py
+++ POO/11_herencia.py
@@ -7,15 +7,15 @@ class Rectangulo:
 	def area(self):
 		return self.base * self.altura
 
-class Cuadrado(Rectangulo):
+class Cuadrado(Rectangulo): # La clase cuadrado extiende rectángulo
 
 	def __init__(self, lado):
-		super().__init__(lado, lado)
+		super().__init__(lado, lado) # referencia directa de la superclase
 
 
 if __name__ == '__main__':
 	rectangulo = Rectangulo(base=3, altura=4)
 	print(rectangulo.area())
 
-	cuadrado = Cuadrado(lado=5)
+	cuadrado = Cuadrado(lado=5)   # Hereda este método de la superclase
 	print(cuadrado.area())
